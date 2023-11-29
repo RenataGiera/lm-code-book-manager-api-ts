db37642d3a7ca235797d28a82d0e3b017eb30c4a
Renata Giera-Dubiel <renatagiera@gmail.com>
2023-11-29 00:30:23 +0000

    feat: add in exception handling to the project

--- src/services/books.ts
+++ src/services/books.ts
@@ -5,25 +5,42 @@ export const getBooks = async () => {
 };
 
 export const getBook = async (bookId: number) => {
+	if (!bookId) {
+		throw new Error(`Book with ID ${bookId} not found.`);
+	}
 	return Book.findOne({
 		where: { bookId },
 	});
+
 };
 
 export const saveBook = async (book: Book) => {
+	const existingBook = await getBook(book.bookId);
+	if (existingBook) {
+		throw new Error(`A book with ID ${book.bookId} already exists.`);
+	}
 	return Book.create<Book>(book);
 };
 
 // User Story 4 - Update Book By Id Solution
 export const updateBook = async (bookId: number, book: Book) => {
+	const existingBook = await getBook(bookId);
+	if (!existingBook) {
+		throw new Error(`Book with ID ${bookId} not found.`);
+	}
 	return Book.update(book, {
 		where: {
 			bookId,
 		},
 	});
 };
-
+// User Story 5 - Delete Book By Id Solution
 export const deleteBook = async (bookId: number) => {
+	const existingBook = await getBook(bookId);
+	if (!existingBook) {
+		throw new Error(`Book with ID ${bookId} not found.`);
+	}
+
 	return Book.destroy ({
 		where: { bookId },
 	});
